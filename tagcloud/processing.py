53f4b661a3b4c8b97300272bf393a3d19adfe359
Arthur Pires <kucapires12@gmail.com>
2018-02-28 23:47:44 -0300

    4 clouds e filtros

--- tagcloud/processing.py
+++ tagcloud/processing.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
-#-*- coding: utf-8 -*-
-# all the imports
+# -*- coding: utf-8 -*-
 import xml.etree.ElementTree as ET
 import re
 
@@ -41,9 +40,11 @@ class pre_processing:
 
             # #convert plain text to utf-8
             # x = unidecode.unidecode(x)
+            x = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','',x)
             x = x.replace(",", "")
             x = x.replace("'", "")
             x = x.replace("\"", "")
+            x = x.replace("\\", "")
             x = x.replace(".", "")
             x = x.replace(":", "")
             x = x.replace("[", "")
@@ -52,8 +53,9 @@ class pre_processing:
             x = x.replace(")", "")
             x = x.replace(";", ".")
             x = x.replace("/", "")
+           
 
-            x = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','',x)
+            
             x = re.sub('\d+','',x)
 
             import sys  
@@ -65,37 +67,45 @@ class pre_processing:
             x = x.replace("á", "a")
             x = x.replace("â", "a")
             x = x.replace("ã", "a")
-            x = x.replace("é", "e")
+            x = x.replace("ã", "a")
+            x = x.replace("è", "e")
             x = x.replace("ê", "e")
             x = x.replace("í", "i")
+            x = x.replace("ì", "i")
             x = x.replace("ó", "o")
             x = x.replace("ô", "o")
             x = x.replace("õ", "o")
+            x = x.replace("ò", "o")
             x = x.replace("ú", "u")    
             x = x.replace("ü", "u")    
             x = x.replace("ç", "c")
+            x = x.replace("ñ", "n")
+
+            x = x.upper()
 
             x = x.replace("À", "A")
             x = x.replace("Á", "A")
             x = x.replace("Â", "A")
             x = x.replace("Ã", "A")
             x = x.replace("É", "E")
+            x = x.replace("È", "E")
             x = x.replace("Ê", "E")
             x = x.replace("Í", "I")
+            x = x.replace("Ì", "I")
             x = x.replace("Ó", "O")
             x = x.replace("Ô", "O")
+            x = x.replace("Ò", "O")
             x = x.replace("Õ", "O")
             x = x.replace("Ú", "U")    
             x = x.replace("Ü", "U")    
             x = x.replace("Ç", "C")
-
-            x = x.upper()
-
+            x = x.replace("Ñ", "N")
+            
 
             if not x.isdigit():
                 
                 split = x.split()
-                stopWords = ['PARA', 'NAO_INFORMADO', 'NAO', 'SIM', 'DE', 'DO', 'DA', 'IMPRESSO', 'COMPLETO', 'PUBLICADO', 'AINDA', 'PELO', 'PELA', 'ENTRE', 'MAIS', 'OUTRO', 'OUTRA', 'SOBRE', 'OUTROS', 'OUTRO', 'OUTRAS', 'BEM', 'CONCLUIDO', 'COMPLETO', 'ENDERECO_RESIDENCIAL', 'LATTES_OFFLINE', 'IMPRESSO', 'RAZOAVELMENTE', 'LIVRE', 'RESUMO', 'EM_ANDAMENTO','POUCO', 'ANTERIOR', 'PARECER', 'QUAL', 'ISSO', 'PODE', '<BR>', '</B>', 'COMO', 'TRABALHO_DE_CONCLUSAO_DE_CURSO_GRADUACAO', 'E/OU', 'QUE']
+                stopWords = ['PARA', 'NAO_INFORMADO', 'NAO', 'SIM', 'DE', 'DO', 'DA', 'IMPRESSO', 'COMPLETO', 'PUBLICADO', 'AINDA', 'PELO', 'PELA', 'ENTRE', 'MAIS', 'OUTRO', 'OUTRA', 'SOBRE', 'OUTROS', 'OUTRO', 'OUTRAS', 'BEM', 'CONCLUIDO', 'COMPLETO', 'ENDERECO_RESIDENCIAL', 'LATTES_OFFLINE', 'IMPRESSO', 'RAZOAVELMENTE', 'LIVRE', 'RESUMO', 'EM_ANDAMENTO','POUCO', 'ANTERIOR', 'PARECER', 'QUAL', 'ISSO', 'PODE', '<BR>', '</B>', '<B>', 'COMO', 'TRABALHO_DE_CONCLUSAO_DE_CURSO_GRADUACAO', 'E/OU', 'QUE', 'DESQUALIFICACAOQUALIFICACAO', 'DOS', 'DDMMAAAA', 'ROGRADEADUNESPBR~TELEDUCPAGINA_INICIALCURSOS_ALLPHP?&TIPO_CURSO=F&COD_PASTA=', 'KROGER', 'S---']
                 removeStop = [w for w in split if (w not in stopWords) and (w.isupper() and len(w) > 2)]
                 # lowerTags = [low for low in split if low.islower() and len(low) > 3]
                 # upperTags = [up for up in split if up.isupper() and len(up) > 2 ]
@@ -122,14 +132,152 @@ class pre_processing:
         tree = ET.parse(self.filename)
         root = tree.getroot()
         atributes = []
-        tags = []
+       
 
         for child in tree.iter('DADOS-GERAIS'):
             name = child.get('NOME-COMPLETO')
-            print name
+            # print name
+        return name
+
+    def getAutores(self):
+
+        tree = ET.parse(self.filename)
+        root = tree.getroot()
+        tags = []
 
         for autor in tree.iter('AUTORES'):
             nomes = autor.get('NOME-COMPLETO-DO-AUTOR')
-            print nomes
+            tags.append(nomes)
 
-        return name
+        result = []
+        for item in tags:
+            if item not in result:
+                 result.append(item)
+
+        for x in result:
+             
+            x = re.sub('\d+','',x)
+
+            import sys  
+
+            reload(sys)  
+            sys.setdefaultencoding('utf8')
+            x = x.replace("'", "")
+            x = x.replace("`", "")
+            x = x.replace("D'A", "")
+            x = x.replace("à", "a")
+            x = x.replace("á", "a")
+            x = x.replace("â", "a")
+            x = x.replace("ã", "a")
+            x = x.replace("ã", "a")
+            x = x.replace("è", "e")
+            x = x.replace("ê", "e")
+            x = x.replace("í", "i")
+            x = x.replace("ì", "i")
+            x = x.replace("ó", "o")
+            x = x.replace("ô", "o")
+            x = x.replace("õ", "o")
+            x = x.replace("ò", "o")
+            x = x.replace("ú", "u")    
+            x = x.replace("ü", "u")    
+            x = x.replace("ç", "c")
+            x = x.replace("ñ", "n")
+
+            x = x.upper()
+
+            
+            x = x.replace("À", "A")
+            x = x.replace("Á", "A")
+            x = x.replace("Â", "A")
+            x = x.replace("Ã", "A")
+            x = x.replace("É", "E")
+            x = x.replace("È", "E")
+            x = x.replace("Ê", "E")
+            x = x.replace("Í", "I")
+            x = x.replace("Ì", "I")
+            x = x.replace("Ó", "O")
+            x = x.replace("Ô", "O")
+            x = x.replace("Ò", "O")
+            x = x.replace("Õ", "O")
+            x = x.replace("Ú", "U")    
+            x = x.replace("Ü", "U")    
+            x = x.replace("Ç", "C")
+            x = x.replace("Ñ", "N")
+
+        print result
+  
+        return result
+        # return response
+
+    def getTitulos(self):
+
+        tree = ET.parse(self.filename)
+        root = tree.getroot()
+        tags = []
+
+        for titulo in tree.iter('DADOS-BASICOS-DO-TRABALHO'):
+            titulos = titulo.get('TITULO-DO-TRABALHO')
+            tags.append(titulos)
+
+        result = []
+        for item in tags:
+            if item not in result:
+                 result.append(item)
+
+        for x in result:
+             
+            x = re.sub('\d+','',x)
+
+            import sys  
+
+            reload(sys)  
+            sys.setdefaultencoding('utf8')
+            x = x.replace("'", "")
+            x = x.replace("`", "")
+            x = x.replace("D'A", "")
+            x = x.replace("à", "a")
+            x = x.replace("á", "a")
+            x = x.replace("â", "a")
+            x = x.replace("ã", "a")
+            x = x.replace("ã", "a")
+            x = x.replace("è", "e")
+            x = x.replace("ê", "e")
+            x = x.replace("í", "i")
+            x = x.replace("ì", "i")
+            x = x.replace("ó", "o")
+            x = x.replace("ô", "o")
+            x = x.replace("õ", "o")
+            x = x.replace("ò", "o")
+            x = x.replace("ú", "u")    
+            x = x.replace("ü", "u")    
+            x = x.replace("ç", "c")
+            x = x.replace("ñ", "n")
+
+            x = x.upper()
+
+
+            x = x.replace("À", "A")
+            x = x.replace("Á", "A")
+            x = x.replace("Â", "A")
+            x = x.replace("Ã", "A")
+            x = x.replace("É", "E")
+            x = x.replace("È", "E")
+            x = x.replace("Ê", "E")
+            x = x.replace("Í", "I")
+            x = x.replace("Ì", "I")
+            x = x.replace("Ó", "O")
+            x = x.replace("Ô", "O")
+            x = x.replace("Ò", "O")
+            x = x.replace("Õ", "O")
+            x = x.replace("Ú", "U")    
+            x = x.replace("Ü", "U")    
+            x = x.replace("Ç", "C")
+            x = x.replace("Ñ", "N")
+
+        print result
+  
+        return result
+
+
+
+
--- tagcloud/tagcloud.py
+++ tagcloud/tagcloud.py
@@ -38,7 +38,7 @@ def upload_file():
     conn = mysql.connect()
     cursor = conn.cursor()
 
-    print request.method
+    # print request.method
     if request.method == 'POST':
 
         file = request.files['file']
@@ -56,17 +56,65 @@ def upload_file():
 
             dateTime = datetime.now() #data atual
 
-            insertNomeArquivo = ("INSERT INTO tagcloud (nome_profissional, data_criado, nome_arquivo) values ('%s', '%s', '%s')" %(name, dateTime.strftime('%Y/%m/%d %H:%M:%S'), str(filename)))
-           
-            cursor.execute(insertNomeArquivo)
+            selectIdExistente = ("SELECT idtagcloud FROM tagcloud WHERE nome_profissional = '%s'" % (name))
+            #pega ids da tabela tagcloud cujo profissional ja exista no banco
+            cursor.execute(selectIdExistente)
+            IdExistente = cursor.fetchall()
 
-            conn.commit()
+            if(len(IdExistente) > 0):
 
-            id_tag = cursor.lastrowid #pega id da ultima insercao
+                IdExistTag = IdExistente[0][0]
 
-            print id_tag
+            selectTagCloud = ("SELECT nome_profissional FROM tagcloud WHERE nome_profissional = '%s'" % (name))
+            #pega o nome do profissionnal que ja existe na tabela tagcloud
+            cursor.execute(selectTagCloud)
+            nomeExist = cursor.fetchall()
+
+            if(len(nomeExist) > 0):
+
+                nomeExistTag = nomeExist[0][0]
+                # print nomeExistTag
+
+                if(nomeExistTag):
+
+                    dropIds = ("DELETE FROM tagcloud_has_palavras WHERE tagcloud_idtagcloud = %d" % (IdExistTag))
+                    dropAutores = ("DELETE FROM autores WHERE tagcloud_idtagcloud = %d" % (IdExistTag))
+                    dropTitulos = ("DELETE FROM titulos WHERE tagcloud_idtagcloud = %d" % (IdExistTag))
+                    #caso ja exista profissional com o mesmo nome deleta todas as palavras associadas a sua tag cloud
+
+                    cursor.execute(dropIds)
+                    cursor.execute(dropAutores)
+                    cursor.execute(dropTitulos)
+
+                    dropArquivo = ("DELETE FROM tagcloud WHERE nome_profissional = '%s'" % (nomeExistTag))
+                    #deleta do banco a tagcloud cujo nome do profissional ja exista
+
+                    cursor.execute(dropArquivo)
+
+                    insertNomeArquivo = ("INSERT INTO tagcloud (nome_profissional, data_criado, nome_arquivo) values ('%s', '%s', '%s')" %(name, dateTime.strftime('%Y/%m/%d %H:%M:%S'), str(filename)))
+                    #depois faz a insercaoo normal
+
+                    cursor.execute(insertNomeArquivo)
+
+                    conn.commit()
+
+                    id_tag = cursor.lastrowid
+
+            else:
+
+                insertNomeArquivo = ("INSERT INTO tagcloud (nome_profissional, data_criado, nome_arquivo) values ('%s', '%s', '%s')" %(name, dateTime.strftime('%Y/%m/%d %H:%M:%S'), str(filename)))
+               
+                cursor.execute(insertNomeArquivo)
+
+                conn.commit()
+
+                id_tag = cursor.lastrowid #pega id da ultima insercao
+
+                # print id_tag
 
         response = pn.readXML()
+
+        
 
         for obj in response: #para cada objeto no json
 
@@ -74,9 +122,9 @@ def upload_file():
             cursor.execute(selectPalavras)
             data = cursor.fetchall()
 
-            print obj['text']
+            # print obj['text']
 
-            print data
+            # print data
 
             if(len(data) > 0):
 
@@ -92,7 +140,7 @@ def upload_file():
 
             else:
 
-                insertPalavra = ("INSERT INTO palavras (text) VALUES ('%s')" % (obj['text'])) 
+                insertPalavra = ("INSERT INTO palavras (text, categoria) VALUES ('%s', 'geral')" % (obj['text'])) 
 
                 cursor.execute(insertPalavra)
 
@@ -104,9 +152,206 @@ def upload_file():
 
                 cursor.execute(insertIntermediario)
 
-                conn.commit()
+                conn.commit() 
 
-    return jsonify(response)  
+        autores = pn.getAutores()
+        for autor in autores:
+
+            insertAutores = ("INSERT INTO autores (autor, tagcloud_idtagcloud) values ('%s', %d)" %(autor, id_tag)) 
+
+            cursor.execute(insertAutores)
+
+            conn.commit()
+
+            id_autor = cursor.lastrowid
+
+            data = cursor.fetchall()
+
+        titulos = pn.getTitulos()
+        for titulo in titulos:
+
+            insertTitulos = ("INSERT INTO titulos (titulo, tagcloud_idtagcloud) values ('%s', %d)" %(titulo, id_tag)) 
+
+            cursor.execute(insertTitulos)
+
+            conn.commit()
+
+            id_titulo = cursor.lastrowid
+
+            data = cursor.fetchall()
+
+
+    return jsonify(response) 
+
+
+
+@app.route('/wordsdb', methods = ['POST'])
+def word_db():
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    getPalavras = ("SELECT * FROM palavras")
+    cursor.execute(getPalavras)
+    palavras = cursor.fetchall()
+
+    words = []
+
+    for x in palavras:
+        words.append({'text': x[1], 'id': x[0]})
+        
+    return jsonify(words)
+
+
+@app.route('/options', methods = ['POST'])
+def options():
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    if request.method == 'POST':
+
+        hello = request.form.getlist('valor[]')
+
+    palavrasID = []
+
+    for x in hello:
+        palavrasID.append(x)
+
+    in_clause = ",".join(palavrasID)
+
+    tagsContem = ("SELECT t.idtagcloud, t.nome_profissional, p.text FROM tagcloud AS t \
+                    INNER JOIN tagcloud_has_palavras AS tp ON (tp.tagcloud_idtagcloud = t.idtagcloud) \
+                    INNER JOIN palavras AS p ON (p.id_palavra = tp.palavras_id_palavra) \
+                    WHERE tp.palavras_id_palavra IN (%s)" % in_clause)
+    cursor.execute(tagsContem)
+    tagclouds = cursor.fetchall()
+
+    ids = { l[0] : [l[0]] for l in tagclouds }  # create a dictionary mapped to each "name" in the CSV
+    names = { l[0] : l[1] for l in tagclouds }  # create a dictionary mapped to each "name" in the CSV
+    for l in tagclouds:
+        if len(ids[l[0]]) > 1:
+            ids[l[0]][1] = ids[l[0]][1]+','+l[2]
+        else:
+            ids[l[0]].append('')
+            ids[l[0]][1] = l[2]
+
+    for key, value in names.iteritems():
+        ids[key].append(value)
+
+    list2 = ids.values()
+
+    return jsonify(list2)
+
+@app.route('/render', methods = ['POST'])
+def renderTag():
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    if request.method == 'POST':
+
+        idtagcloud = request.form['valor']
+
+        tagcloud = ("SELECT p.text, tp.size FROM palavras AS p \
+                    INNER JOIN tagcloud_has_palavras AS tp ON (tp.palavras_id_palavra = p.id_palavra) \
+                    WHERE  tp.tagcloud_idtagcloud = %d ORDER BY tp.size DESC" % int(idtagcloud))
+
+        cursor.execute(tagcloud)
+        cloud = cursor.fetchall()
+
+        # print cloud
+        response = []
+        
+
+        for item in cloud:
+            response.append({'text': item[0], 'size': item[1]})
+
+        # print response
+        return jsonify(response)
+
+@app.route('/listagem', methods = ['POST'])
+def listar():
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    getTags = ("SELECT * FROM tagcloud")
+    cursor.execute(getTags)
+    clouds = cursor.fetchall()
+
+    words = []
+
+    for x in clouds:
+        words.append(x)
+
+    # print words
+
+    return jsonify(words)
+
+
+@app.route('/autores', methods = ['POST'])
+def autores():
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    if request.method == 'POST':
+
+        idtagcloud = request.form['valor']
+        print idtagcloud
+
+    getAutores = ("SELECT *  FROM autores AS a INNER JOIN tagcloud AS t ON (t.idtagcloud = a.tagcloud_idtagcloud) WHERE t.idtagcloud = %d" %int(idtagcloud)) 
+    
+    print getAutores
+
+    cursor.execute(getAutores)
+    conn.commit()
+    autores = cursor.fetchall()
+
+    autor = []
+
+    for x in autores:
+        autor.append(x)
+
+    print autor
+
+    return jsonify(autor)
+
+@app.route('/titulos', methods = ['POST'])
+def titulos():
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    if request.method == 'POST':
+
+        idtagcloud = request.form['valor']
+        print idtagcloud
+
+    getTitulos = ("SELECT *  FROM titulos AS a INNER JOIN tagcloud AS t ON (t.idtagcloud = a.tagcloud_idtagcloud) WHERE t.idtagcloud = %d" %int(idtagcloud)) 
+    
+    print getTitulos
+
+    cursor.execute(getTitulos)
+    conn.commit()
+    titulos = cursor.fetchall()
+
+    titulo = []
+
+    for x in titulos:
+        titulo.append(x)
+
+    print titulo
+
+    return jsonify(titulo)
+
+
+
+
+
+
+
 
 
 
